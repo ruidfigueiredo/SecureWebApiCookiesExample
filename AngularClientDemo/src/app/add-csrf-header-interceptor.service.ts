238b3e3ef956f507b02f46ba145c2596bcd0d9c1
Rui Figueiredo <rui@blinkingcaret.com>
2018-11-30 23:17:13 +0000

    getCookieValue

--- AngularClientDemo/src/app/add-csrf-header-interceptor.service.ts
+++ AngularClientDemo/src/app/add-csrf-header-interceptor.service.ts
@@ -5,23 +5,18 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AddCsrfHeaderInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        var requestToken = this.getCookie("XSRF-REQUEST-TOKEN");
-        return next.handle(req.clone({            
+        var requestToken = this.getCookieValue("XSRF-REQUEST-TOKEN");
+        return next.handle(req.clone({
             headers: req.headers.set("X-XSRF-TOKEN", requestToken)
         }));
     }
-    
-    private getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
+
+    private getCookieValue(cookieName: string) {
+        const allCookies = decodeURIComponent(document.cookie).split("; ");
+        for (let i = 0; i < allCookies.length; i++) {
+            const cookie = allCookies[i];
+            if (cookie.startsWith(cookieName + "=")){
+                return cookie.substring(cookieName.length + 1);
             }
         }
         return "";
